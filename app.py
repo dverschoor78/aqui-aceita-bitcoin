946ab2577ca52a2f60a7f9806ad9a9c6998c5e7a
JVX <jaimevbarros@gmail.com>
2025-05-21 10:34:08 -0300

    change to port 5000

--- app.py
+++ app.py
@@ -103,4 +103,4 @@ def listar_estabelecimentos():
 
 if __name__ == '__main__':
     init_db()
-    app.run(host='0.0.0.0', port=3939, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
